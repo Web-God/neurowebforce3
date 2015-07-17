d0b02b7e9f8939014cc6ab8b5604f05aa949b25f
Web-God <alvardsandz@gmail.com>
2015-07-17 11:46:12 +0200

    publish work
    original css directory

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Git Experience</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
-deserunt similique voluptatum, magni itaque, laborum quisquam a quibusdam animi sunt reprehenderit
-culpa veritatis magnam unde nam hic, mollitia voluptatem. Voluptate.
+		deserunt similique voluptatum, magni itaque, laborum quisquam a quibusdam animi sunt reprehenderit
+		culpa veritatis magnam unde nam hic, mollitia voluptatem. Voluptate.
 	</p>
 </body>
 </html>
